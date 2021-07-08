cb3ef11f9ebeff18ed354ed6949a4a25d08321cd
Yasamin Jafarian <yasamin@umn.edu>
2021-07-08 11:32:27 -0500

    Update make_video.m

--- MATLAB_Visualization/make_video.m
+++ MATLAB_Visualization/make_video.m
@@ -16,6 +16,9 @@ remove_images = true;
 % video. remove_images = true will remove the generated images. If you
 % don not wish so set it as false.
 
+pred_path = [path,'infer_out/'];
+
+%% making the video
 %% making the video
 tic
 color_names = {'gray','color'};
@@ -28,7 +31,7 @@ for i = 1:2
     fprintf(['processing frame ', name , ' for ',color_name,' scale.\n']);
 
 
-    d = dlmread([path,'infer_out/',name,'.txt'])-1.5;
+    d = dlmread([pred_path,name,'.txt'])-1.5;
 
     m = d>0;
     m = remove_n_boundary(m,2);
@@ -82,23 +85,23 @@ for i = 1:2
     als2 = [-(90:-0.2:90-angl2),-(90-angl2:0.2:90-1)];
     als = [als1,als2]';
     azs = [0*ones(size(als1)),0*ones(size(als2))]';
-    mkdir([path,'infer_out/video/',name,'/',color_name,'/'])
-    ims1 =CaptureFigVid([azs,als], [path,'infer_out/video/',name,'/',color_name,'/'],name,color_name,OptionZ);
+    mkdir([pred_path,'video/',name,'/',color_name,'/'])
+    ims1 =CaptureFigVid([azs,als], [pred_path,'video/',name,'/',color_name,'/'],name,color_name,OptionZ);
 end
 delete TEST.jpg
 %% make the combined gray and color images
-d = dir([path,'infer_out/video/',name,'/','color','/*.jpg']);
-mkdir([path,'infer_out/video/',name,'/','gray_color'])
+d = dir([pred_path,'video/',name,'/','color','/*.jpg']);
+mkdir([pred_path,'video/',name,'/','gray_color'])
 for i =  1:length(d)
-    im1 = imread([path,'infer_out/video/',name,'/','gray','/',int2str(i),'.jpg']);
-    im2 = imread([path,'infer_out/video/',name,'/','color','/',int2str(i),'.jpg']);
+    im1 = imread([pred_path,'video/',name,'/','gray','/',int2str(i),'.jpg']);
+    im2 = imread([pred_path,'video/',name,'/','color','/',int2str(i),'.jpg']);
     im = [im1,im2];
-    imwrite(im,[path,'infer_out/video/',name,'/','gray_color/',int2str(i),'.jpg'])
+    imwrite(im,[pred_path,'video/',name,'/','gray_color/',int2str(i),'.jpg'])
 end
 
 %% convert the image sequence to video.
-workingDir = [path,'infer_out/video/',name,'/','gray_color','/'];
-out_dir = [path,'infer_out/video/',name,'/'];
+workingDir = [pred_path,'video/',name,'/','gray_color','/'];
+out_dir = [pred_path,'video/',name,'/'];
 imageNames = dir(fullfile(workingDir,'*.jpg'));
 imageNames = {imageNames.name}';
 outputVideo = VideoWriter([out_dir,'video.avi']);
@@ -112,8 +115,8 @@ close(outputVideo)
 
 %% remove the image files
 if remove_images  
-    rmdir([path,'infer_out/video/',name,'/','gray_color'],'s')
-    rmdir([path,'infer_out/video/',name,'/','gray'],'s')
-    rmdir([path,'infer_out/video/',name,'/','color'],'s')
+    rmdir([pred_path,'video/',name,'/','gray_color'],'s')
+    rmdir([pred_path,'video/',name,'/','gray'],'s')
+    rmdir([pred_path,'video/',name,'/','color'],'s')
 end
 toc
