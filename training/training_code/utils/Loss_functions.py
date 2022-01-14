c08f2c7930ce6b06dc5b15d6ee85805ff773fe16
MoonByeongPil <mbpil7044@postech.ac.kr>
2022-01-14 16:02:45 +0900

    main

--- training/training_code/utils/Loss_functions.py
+++ training/training_code/utils/Loss_functions.py
@@ -13,38 +13,38 @@ IMAGE_WIDTH = 256
 
 # *****************************************************************************************************
 
-def calc_loss(output, y, z_r):
+def calc_loss(output, y, z_r):  #y와 output의 행렬 shape가 같아야 할 것으로 보임, 결국 y와 output의 차이를 비교.
     
     # y refine
-    y_masked = tf.where(z_r, y, 0*tf.ones_like(y))
-    y_masked_flat_refined = tf.reshape(y_masked,[-1, IMAGE_HEIGHT*IMAGE_WIDTH])
+    y_masked = tf.where(z_r, y, 0*tf.ones_like(y))  #bool 타입 z_r, 이 행렬에서 true값이 들어가 있는 곳은 y를 참조하고 false가 들어가 있는 곳은 0값으로 넣어버림.//tf.ones_like(y)는 y배열의 shape와 같은데 내용물이 1인 행렬
+    y_masked_flat_refined = tf.reshape(y_masked,[-1, IMAGE_HEIGHT*IMAGE_WIDTH]) #위 마스크된 결과 reshape할 것. 높이축은 임의로, 가로축 넓이는 이미지 입력 높이*너비
     
     # output refine
-    o_masked = tf.where(z_r, output, 0*tf.ones_like(y))
-    o_masked_flat_refined = tf.reshape(o_masked,[-1, IMAGE_HEIGHT*IMAGE_WIDTH])
+    o_masked = tf.where(z_r, output, 0*tf.ones_like(y)) #bool 타입z_r에 의해서 true인 부분은 output 넣고 false면 0넣음
+    o_masked_flat_refined = tf.reshape(o_masked,[-1, IMAGE_HEIGHT*IMAGE_WIDTH]) #위에서 만든 행렬의 모양을 변경, 너비를 높이*너비로 고정. (y_masked와 동일)
     
-    # mask refine
-    mask_one_refined = tf.where(z_r, tf.ones_like(y), 0*tf.ones_like(y))
-    mask_one_flat_refined = tf.reshape(mask_one_refined,[-1, IMAGE_HEIGHT*IMAGE_WIDTH])
+    # mask refine ==> binary하게 refine즉, 바이너리 마스크다.
+    mask_one_refined = tf.where(z_r, tf.ones_like(y), 0*tf.ones_like(y))    #z_r true면 1, false면 0.
+    mask_one_flat_refined = tf.reshape(mask_one_refined,[-1, IMAGE_HEIGHT*IMAGE_WIDTH]) #마찬가지로, 너비를 높이*너비로 고정한다. (reshape)
     
     # num of pixels
-    numOfPix = tf.reduce_sum(mask_one_flat_refined,1)
+    numOfPix = tf.reduce_sum(mask_one_flat_refined,1)   #mask_one_flat_refined를 행 단위로 sum, 즉, numOfPix의 각 행은 하나의 이미지 넓이에 대해서 1의 개수를 센다. 
     
-    d = tf.subtract(o_masked_flat_refined, y_masked_flat_refined)
-    d_sum = tf.reduce_sum(tf.square(d),1)
+    d = tf.subtract(o_masked_flat_refined, y_masked_flat_refined)   #z_r이 true인 부분에 대해서는 output-y를 대입, false인 부분에 대해서는 0 대입.
+    d_sum = tf.reduce_sum(tf.square(d),1)   #위의 각 요소별 제곱을 한 후에 행 단위로 합침. 각 행별로 더하므로 
     
     cost = tf.reduce_mean(tf.truediv(d_sum, numOfPix))
     return cost
 
 # *****************************************************************************************************
 
-def calc_loss_normal(output, y_normal,z_refined):
+def calc_loss_normal(output, y_normal,z_refined):   #Normal에 대한 loss를 계산 :4차원 텐서인가? ==> 아래에 reduce_sum이 axis=3에 대해 수행됨.
 
     # gives mean angle error for given output tensor and its ref y
-    output_mask = tf.abs(output) < 1e-5
-    output_no0 = tf.where(output_mask, 1e-5*tf.ones_like(output), output)
-    output_mag = tf.expand_dims(tf.sqrt(tf.reduce_sum(tf.square(output_no0),3)),-1)
-    output_unit = tf.divide(output_no0,output_mag)
+    output_mask = tf.abs(output) < 1e-5 #output의 절댓값이 10의 -5승보다 작으면 mask에 bool로 저장.
+    output_no0 = tf.where(output_mask, 1e-5*tf.ones_like(output), output)   #위의 마스크가 true인 경우에는 10 -5승을 입력으로 넣어줌. false면 output의 값을 넣어줌.
+    output_mag = tf.expand_dims(tf.sqrt(tf.reduce_sum(tf.square(output_no0),3)),-1) #위의 행렬 요소 제곱 -> 3이라는 축(4번째로 큰 축=1차원)으로 더해줌 최소 4차원-> 모든 요소에 root. -1은 가장 안쪽 차원 추가(가장 작은 차원)
+    output_unit = tf.divide(output_no0,output_mag)  #요소별 나눗셈 진행 ==> 말 그대로 normalize
 
     z_mask = z_refined[...,0]
     a11 = tf.boolean_mask(tf.reduce_sum(tf.square(output_unit),3),z_mask)
